517d1b53091a6562f6e304fb8b214705ae4691dc
Fatimaezzahrarb <north.winds04@gmail.com>
2023-10-03 17:51:33 -0700

    Python

--- 0x09-python-everything_is_object/tests/101-main.py
+++ 0x09-python-everything_is_object/tests/101-main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-LockedClass = __import__('101-locked_class').LockedClass
+"""
+Define AN empty class
+"""
 
-lc = LockedClass()
-lc.first_name = "John"
-try:
-    lc.last_name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+
+class LockedClass:
+    """
+    Locked class.
+    """
+    __slots__ = ["first_name"]
+    pass
