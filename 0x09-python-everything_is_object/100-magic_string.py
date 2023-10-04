7742d2e9de131591cec5b5c44640643af94756e3
Fatimaezzahrarb <north.winds04@gmail.com>
2023-10-03 18:18:03 -0700

    Python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(c=0):
-    magic_string.c = getattr(magic_string, "c", 0) + 1
-    return((("Holberton, " * magic_string.c))[:-2])
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Defines the lock class."""
+
+
+class LockedClass:
+    """
+    Prevent  user from instantiating new LockedClass attributes
+    for anything BUT attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
